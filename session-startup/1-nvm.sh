5b94251369c6c5f57ba0cce17ac117d7c6adc9fc
Aurélien Bourdon <aurelien.bourdon@gmail.com>
2018-05-03 21:00:32 +0200

    feat(nvm): "official" nvm loading process

--- session-startup/1-nvm.sh
+++ session-startup/1-nvm.sh
@@ -3,6 +3,6 @@
 # Example of a simple nvm (https://github.com/creationix/nvm) initialization process
 
 # Adapt to your need
-export NVM_DIR=$HOME/.nvm
+export NVM_DIR="$HOME/.nvm"
 
-source $(brew --prefix nvm)/nvm.sh
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
